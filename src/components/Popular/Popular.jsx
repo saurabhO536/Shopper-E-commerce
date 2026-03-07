942ac0bbc5e7abe3722b542ab15a71da7f75a4cd
DIPAKK2310 <dipakkhare217@gmail.com>
2026-03-07 18:26:16 +0000

    feat:add tailwindcss and made it resposive

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -4,29 +4,30 @@ import Item from '../Item/Item'
 
 const Popular = () => {
   return (
-    <div className="flex flex-col items-center gap-6 py-12 px-6">
-      <h2 className="text-[#171717] text-3xl md:text-5xl font-semibold">
+    <section className="w-full flex flex-col items-center py-12 px-4 sm:mt-10">
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-gray-900 text-center">
         Popular In Women
       </h2>
 
-      <hr className="w-[120px] h-[4px] rounded-full bg-gray-800" />
+      <hr className="w-20 sm:w-28 h-[3px] bg-gray-800 rounded-full mt-3 mb-10" />
 
       <div
         className="
-        grid
-        grid-cols-1
-        sm:grid-cols-2
-        md:grid-cols-3
-        lg:grid-cols-4
-        gap-6
-        mt-10
+         w-full
+  max-w-7xl
+  grid
+  grid-cols-1
+  sm:grid-cols-2
+  lg:grid-cols-4
+  gap-6
+  
       "
       >
-        {data_product.map((item, i) => (
-          <Item key={i} {...item} />
+        {data_product.map((item) => (
+          <Item key={item.id} {...item} />
         ))}
       </div>
-    </div>
+    </section>
   )
 }
 
